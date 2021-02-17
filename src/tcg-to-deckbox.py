8f7bb6a5bc7fc52b0779e60c6efa84a799515d98
ThePieBandit <61213359+ThePieBandit@users.noreply.github.com>
2021-02-17 01:23:06 -0500

    Some linting fixes - still a ways to go

--- src/tcg-to-deckbox.py
+++ src/tcg-to-deckbox.py
@@ -1,13 +1,15 @@
 import sys
 import csv
 import time
-import os, os.path
+import os
+import os.path
 import configparser
 import re
 from tkinter import filedialog
 from tkinter import messagebox
 import tkinter as tk
-import urllib.request, json 
+import urllib.request
+import json
 
 
 # Constants
@@ -15,28 +17,31 @@ MULTI_NAMES_FILE = "multiple_names.json"
 SCRYFALL_URL = "https://api.scryfall.com/cards/search?order=cmc&q=%28is%3Adoublesided%20OR%20is%3Asplit%20OR%20is%3Aadventure%29%20%20AND%20game%3Apaper%20AND%20-is%3Atoken%20AND%20-set%3ACMB1"
 
 #global vars
-scryfall_data={}
-
+scryfall_data = {}
 
 
 # Get rid of the root TK window, we don't need it.
 root = tk.Tk()
 root.withdraw()
 
-def fetch_multiple_names(uri,page=1):
-    print('Begin: Download %s, page %s of results' % (uri, page) )
+
+def fetch_multiple_names(uri, page=1):
+    print('Begin: Download %s, page %s of results' % (uri, page))
     with urllib.request.urlopen(uri) as url:
         tmp_scryfall_data = json.loads(url.read().decode())
-        
+
         for x in tmp_scryfall_data["data"]:
-            scryfall_data[x["card_faces"][0]["name"]] = x["name"] 
+            scryfall_data[x["card_faces"][0]["name"]] = x["name"]
         if "next_page" in tmp_scryfall_data:
             fetch_multiple_names(tmp_scryfall_data["next_page"], page + 1)
 
 # Utility function to replace strings in the csv from the replacements.config file.
+
+
 def replace_strings(dict, replacementSection, columnName):
     if dict[columnName].lower() in configParser[replacementSection].keys():
-        dict[columnName]=configParser[replacementSection][dict[columnName].lower()]
+        dict[columnName] = configParser[replacementSection][dict[columnName].lower()]
+
 
 def getPathPrefix():
     try:
@@ -47,10 +52,12 @@ def getPathPrefix():
         prefix = os.path.abspath(".")
     return prefix
 
-#Check to see if we have DFC/Split/etc card names from scryfall and if it is up to date
+
+# Check to see if we have DFC/Split/etc card names from scryfall and if it is up to date
 try:
     multi_files_last_updated = os.path.getmtime(MULTI_NAMES_FILE)
-    print("%s last modified: %s" % (MULTI_NAMES_FILE,time.ctime(multi_files_last_updated)))
+    print("%s last modified: %s" %
+          (MULTI_NAMES_FILE, time.ctime(multi_files_last_updated)))
     now = time.time()
     last_week = now - 60*60*24*7
     if multi_files_last_updated < last_week:
@@ -59,7 +66,7 @@ try:
         with open(MULTI_NAMES_FILE, 'w') as multiple_names:
             json.dump(scryfall_data, multiple_names)
         print("Done!")
-    else: 
+    else:
         print("Using existing %s file..." % MULTI_NAMES_FILE)
         with open(MULTI_NAMES_FILE) as multiple_names:
             scryfall_data = json.load(multiple_names)
@@ -73,32 +80,35 @@ except Exception:
     print("Done!")
 
 
-
 # Get our input
-GUI=False
+GUI = False
 if len(sys.argv) < 2:
-    GUI=True
-    FILE=filedialog.askopenfilename(title="Select your TCGPlayer app export file",filetypes=[("TCGPlayer exports", ".csv"),("All files","*.*")])
+    GUI = True
+    FILE = filedialog.askopenfilename(title="Select your TCGPlayer app export file", filetypes=[
+        ("TCGPlayer exports", ".csv"), ("All files", "*.*")])
     if len(FILE) == 0:
-        messagebox.showerror(title="Input file not provided",message="You must pass the TCGPlayer csv export file to this program.")
+        messagebox.showerror(title="Input file not provided",
+                             message="You must pass the TCGPlayer csv export file to this program.")
         sys.exit()
 else:
-    FILE=sys.argv[1]
+    FILE = sys.argv[1]
 
-skipcolumns=["Simple Name","Set Code","Rarity","Product ID","SKU","Price","Price Each"]
-outputFile="deckbox_import.csv"
+skipcolumns = ["Simple Name", "Set Code", "Rarity",
+               "Product ID", "SKU", "Price", "Price Each"]
+outputFile = "deckbox_import.csv"
 
 configParser = configparser.ConfigParser(delimiters="=")
-configParser.read(os.path.join(getPathPrefix(),"replacements.config"))
+configParser.read(os.path.join(getPathPrefix(), "replacements.config"))
 
-with open(FILE, newline="") as tcgcsvfile,open(outputFile, "w", newline="") as deckboxcsvfile:
+with open(FILE, newline="") as tcgcsvfile, open(outputFile, "w", newline="") as deckboxcsvfile:
 
     try:
-        csv.Sniffer().sniff(tcgcsvfile.read(4096),delimiters=",")
+        csv.Sniffer().sniff(tcgcsvfile.read(4096), delimiters=",")
         tcgcsvfile.seek(0)
     except:
         if GUI:
-            messagebox.showerror(title="Invalid input file",message="The file selected does not appear to be a valid CSV file.")
+            messagebox.showerror(
+                title="Invalid input file", message="The file selected does not appear to be a valid CSV file.")
         else:
             print("The file passed does not appear to be a valid CSV file.")
         sys.exit()
@@ -109,21 +119,22 @@ with open(FILE, newline="") as tcgcsvfile,open(outputFile, "w", newline="") as d
     headerstcg = csvreader.fieldnames
     for index, header in enumerate(headerstcg):
         if header.lower() in configParser["COLUMNS"].keys():
-            headerstcg[index]=configParser["COLUMNS"][header.lower()]
+            headerstcg[index] = configParser["COLUMNS"][header.lower()]
 
     # Unnecessary Columns: Simple Name,Set Code,Printing,Rarity,Product ID,SKU,Price,Price Each.
-    headersdeckbox=[x for x in headerstcg if x not in skipcolumns]
+    headersdeckbox = [x for x in headerstcg if x not in skipcolumns]
 
-    csvwriter  = csv.DictWriter(deckboxcsvfile, quoting=csv.QUOTE_ALL, fieldnames=headersdeckbox)
+    csvwriter = csv.DictWriter(
+        deckboxcsvfile, quoting=csv.QUOTE_ALL, fieldnames=headersdeckbox)
     csvwriter.writeheader()
     for row in csvreader:
         # Don't bother with columns that are going to be ignored anyways
         for skippable in skipcolumns:
-            row.pop(skippable,'')
+            row.pop(skippable, '')
 
         # Map the printing column to the Foil column
         if row["Foil"] == "Normal":
-            row["Foil"]=""
+            row["Foil"] = ""
 
         # Map Card Condition
         replace_strings(row, "CONDITIONS", "Condition")
@@ -132,27 +143,25 @@ with open(FILE, newline="") as tcgcsvfile,open(outputFile, "w", newline="") as d
         replace_strings(row, "LANGUAGES", "Language")
 
         # Map Specific Card Names, and drop extra tidbits
-        row["Name"]=row["Name"].replace(" (Alternate Art)","")
-        row["Name"]=row["Name"].replace(" (Extended Art)","")
-        row["Name"]=row["Name"].replace(" (Showcase)","")
-        row["Name"]=row["Name"].replace(" (Borderless)","")
-        row["Name"]=row["Name"].replace(" (Stained Glass)","")
-        row["Name"]=row["Name"].replace(" (Etched Foil)","")
-        #For BFZ lands...there's no differentiator from the full arts and the non full arts.
-        row["Name"]=row["Name"].replace(" - Full Art","")
+        row["Name"] = row["Name"].replace(" (Alternate Art)", "")
+        row["Name"] = row["Name"].replace(" (Extended Art)", "")
+        row["Name"] = row["Name"].replace(" (Showcase)", "")
+        row["Name"] = row["Name"].replace(" (Borderless)", "")
+        row["Name"] = row["Name"].replace(" (Stained Glass)", "")
+        row["Name"] = row["Name"].replace(" (Etched Foil)", "")
+        # For BFZ lands...there's no differentiator from the full arts and the non full arts.
+        row["Name"] = row["Name"].replace(" - Full Art", "")
 
-        #Very specifc conditons
+        # Very specifc conditons
         if row["Name"].contains("(JP Alternate Art)") and row["Edition"] == "War of the Spark":
             row["Edition"] = "War of the Spark Japanese Alternate Art"
-            row["Name"]=row["Name"].replace(" (JP Alternate Art)","")
-
+            row["Name"] = row["Name"].replace(" (JP Alternate Art)", "")
 
         # Remove numbers, mostly for lands, but for some other special cases (M21 Teferi)
         row["Name"] = re.sub(r" \(\d+\)", "", row["Name"])
         replace_strings(row, "NAMES", "Name")
         if row["Name"] in scryfall_data:
-            row["Name"]=scryfall_data[row["Name"]]
-
+            row["Name"] = scryfall_data[row["Name"]]
 
         # remove weird symbols from card numbers
         row["Card Number"] = re.sub(r"[*★]", "", row["Card Number"])
@@ -164,8 +173,10 @@ with open(FILE, newline="") as tcgcsvfile,open(outputFile, "w", newline="") as d
         csvwriter.writerow(row)
 
 # All Done!
-successMsg="Your import file for deckbox.org is available here: %s" % os.path.abspath(outputFile)
+successMsg = "Your import file for deckbox.org is available here: %s" % os.path.abspath(
+    outputFile)
 if GUI:
-    messagebox.showinfo(title="Conversion completed successfully!", message=successMsg)
+    messagebox.showinfo(
+        title="Conversion completed successfully!", message=successMsg)
 else:
     print(successMsg)

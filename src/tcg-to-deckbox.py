caa6f093c5ac7d54ebf04dad18faeb1a8d36ae80
Robert Konell <robert.konell@gmail.com>
2021-12-29 23:44:24 -0800

    Added new replacement rules, added ignore rules for dualfaced cards that deckbox uses the front face name, removed art cards from scryfall query

--- src/tcg-to-deckbox.py
+++ src/tcg-to-deckbox.py
@@ -15,7 +15,9 @@ import json
 
 # Constants
 MULTI_NAMES_FILE = "multiple_names.json"
-SCRYFALL_URL = "https://api.scryfall.com/cards/search?order=cmc&q=%28is%3Adoublesided%20OR%20is%3Asplit%20OR%20is%3Aadventure%29%20%20AND%20game%3Apaper%20AND%20-is%3Atoken%20AND%20-set%3ACMB1"
+SCRYFALL_URL = "https://api.scryfall.com/cards/search?order=cmc&q=%28is%3Adoublesided%20OR%20is%3Asplit%20OR%20is%3Aadventure%29%20%20AND%20game%3Apaper%20AND%20-is%3Atoken%20AND%20-set%3ACMB1%20AND%20-is%3Aextra"
+# some dual faced cards are referenced using only the front name on deckbox
+MULTI_NAMES_IGNORE = ["Nicol Bolas, the Ravager","Hadana's Climb"]
 
 #global vars
 scryfall_data = {}
@@ -33,7 +35,11 @@ def fetch_multiple_names(uri, page=1):
             tmp_scryfall_data = response.json()
 
             for x in tmp_scryfall_data["data"]:
-                scryfall_data[x["card_faces"][0]["name"]] = x["name"]
+                if x["card_faces"][0]["name"] in MULTI_NAMES_IGNORE:
+                    # detected a card we want to ignore from scryfall
+                    continue
+                else:
+                    scryfall_data[x["card_faces"][0]["name"]] = x["name"]
             if "next_page" in tmp_scryfall_data:
                 fetch_multiple_names(tmp_scryfall_data["next_page"], page + 1)
     except Exception:
@@ -155,6 +161,22 @@ with open(FILE, newline="") as tcgcsvfile, open(outputFile, "w", newline="") as 
         row["Name"] = row["Name"].replace(" (Stained Glass)", "")
         row["Name"] = row["Name"].replace(" (Etched Foil)", "")
         row["Name"] = row["Name"].replace(" (Foil Etched)", "")
+        row["Name"] = row["Name"].replace(" (Dungeon Module)", "")
+        row["Name"] = row["Name"].replace(" (CHAMPS)", "")
+        row["Name"] = row["Name"].replace(" (JP Alternate Art)", "")
+        row["Name"] = row["Name"].replace(" (No PW Symbol)", "")
+        row["Name"] = row["Name"].replace(" (Retro Frame)", "")
+        row["Name"] = row["Name"].replace(" (Phyrexian)", "")
+        row["Name"] = row["Name"].replace(" (Bring a Friend Promo)", "")
+
+        # alternative art/name
+        row["Name"] = row["Name"].replace("Sisters of the Undead - ", "")
+        row["Name"] = row["Name"].replace("Mina Harker - ", "")
+        row["Name"] = row["Name"].replace("Abraham Van Helsing - ", "")
+        row["Name"] = row["Name"].replace("Dracula, Lord of Blood - ", "")
+        row["Name"] = row["Name"].replace("Dracula the Voyager - ", "")
+        row["Name"] = row["Name"].replace("Dracula, Blood Immortal - ", "")
+
         # For BFZ lands...there's no differentiator from the full arts and the non full arts.
         row["Name"] = row["Name"].replace(" - Full Art", "")
 
